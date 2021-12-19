b7826a80aec47a3289bcfbf813a63af56f2d1020
tintukj001 <tintujohney@gmail.com>
2021-12-19 13:06:57 -0500

    test purpose

---
+++ src/PRACTICE.java
@@ -0,0 +1,13 @@
+
+public class PRACTICE {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=12;
+		int c=a+b;
+		int d=a-b;
+		System.out.println(d);
+	}
+	
+
+}
---
+++ src/Practice2.java
@@ -0,0 +1,19 @@
+
+public class Practice2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String t;
+t= "thanksgiving";
+
+int a=10;
+int b=25; 
+double c=50;
+boolean m= true;
+
+
+
+System.out.println(a+c-b+ t + m);
+	}
+
+}
